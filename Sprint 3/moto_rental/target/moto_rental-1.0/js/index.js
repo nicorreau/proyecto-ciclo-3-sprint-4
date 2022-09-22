424ea5504c053f227f74302da90562f1046dfebd
nicorreau <nicorreau@hotmail.com>
2022-09-21 21:03:39 -0500

    ASe añade clases controller y archivo js (se intenta empezar)

---
+++ Sprint 3/moto_rental/target/moto_rental-1.0/js/index.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    $("#form-login").submit(function (event) {
+
+        event.preventDefault();
+        autenticarUsuario();
+    });
+
+    $("#form-register").submit(function (event) {
+
+        event.preventDefault();
+        registrarUsuario();
+    });
+
+
+});
+
+function autenticarUsuario() {
+
+    let username = $("#usuario").val();
+    let contrasena = $("#contrasena").val();
+
+    $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletUsuarioLogin",
+        data: $.param({
+            username: username,
+            contrasena: contrasena
+        }),
+        success: function (result) {
+            let parsedResult = JSON.parse(result);
+            if (parsedResult != false) {
+                $("#login-error").addClass("d-none");
+                let username = parsedResult['username'];
+                document.location.href = "home.html?username=" + username;
+            } else {
+                $("#login-error").removeClass("d-none");
+            }
+        }
+    });
+}
+
